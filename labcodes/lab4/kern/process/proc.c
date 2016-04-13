d35fe38890719a0c6f4c213ef06dc6e118d41331
l1xiao <shutianhe@gmail.com>
2016-04-13 22:35:48 +0800

    finish lab4

--- labcodes/lab4/kern/process/proc.c
+++ labcodes/lab4/kern/process/proc.c
@@ -102,6 +102,18 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
+        proc->state = PROC_UNINIT;
+        proc->pid = -1;
+        proc->runs = 0;
+        proc->kstack = 0;
+        proc->need_resched = 0;
+        proc->parent = NULL;
+        proc->mm = NULL;
+        memset(&(proc->context), 0, sizeof(struct context));
+        proc->tf = NULL;
+        proc->cr3 = boot_cr3;
+        proc->flags = 0;
+        memset(proc->name, 0, PROC_NAME_LEN);
     }
     return proc;
 }
@@ -289,13 +301,37 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
      *   nr_process:   the number of process set
      */
 
-    //    1. call alloc_proc to allocate a proc_struct
-    //    2. call setup_kstack to allocate a kernel stack for child process
-    //    3. call copy_mm to dup OR share mm according clone_flag
-    //    4. call copy_thread to setup tf & context in proc_struct
-    //    5. insert proc_struct into hash_list && proc_list
-    //    6. call wakeup_proc to make the new child process RUNNABLE
-    //    7. set ret vaule using child proc's pid
+    // 1. call alloc_proc to allocate a proc_struct
+	if ((proc = alloc_proc()) == NULL) {
+		goto fork_out;
+	}
+	proc->parent = current;
+	// 2. call setup_kstack to allocate a kernel stack for child process		
+	if (setup_kstack(proc) != 0) {
+		goto bad_fork_cleanup_kstack;
+	}
+	// 3. call copy_mm to dup OR share mm according clone_flag
+	if (copy_mm(clone_flags, proc) != 0) {
+		goto bad_fork_cleanup_proc;
+	}
+    // 4. call copy_thread to setup tf & context in proc_struct
+	copy_thread(proc, stack, tf);
+    // 5. insert proc_struct into hash_list && proc_list
+    
+	bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        proc->pid = get_pid();
+        hash_proc(proc);
+        list_add(&proc_list, &(proc->list_link));
+        nr_process ++;
+    }
+    local_intr_restore(intr_flag);
+
+    wakeup_proc(proc);
+
+    ret = proc->pid;
+	
 fork_out:
     return ret;
 
